88c4ab9791263156f8483485193053101d3fa15d
lanlana <1600012874@pku.edu.cn>
2023-05-24 15:56:10 +0800

    add function eval_sentence_obj

--- src/baseline/retriever/eval_sentence_retriever.py
+++ src/baseline/retriever/eval_sentence_retriever.py
@@ -36,24 +36,58 @@ def clean_title(text):
     )
     return text
 
+def eval_sentence_obj(preds, split):
+    coverage = []
+    coverage_all = []
+    annotation_processor = AnnotationProcessor('data/{}.jsonl'.format(split))
+    annotation_by_id = {el.get_id(): el for el in annotation_processor if
+                            el.has_evidence() and el.get_evidence_type(flat=True) == EvidenceType.SENTENCE}
+
+    # print(annotation_by_id.keys())
+
+    for idx, js in enumerate(preds):
+        id = js['id']
+        if id not in annotation_by_id:
+            continue
+        anno = annotation_by_id[id]
+        docs_gold = list(set([t for t in anno.get_evidence(flat=True) if '_sentence_' in t]))
+        if len(docs_gold) == 0:
+            continue
+        # print(docs_gold)
+        docs_predicted = js['predicted_sentences'][:5]
+        # docs_predicted =  [t[0] + '_' + t[1].replace('s_', 'sentence_') for t in js['predicted_sentences']]
+        if anno.get_verdict() in ['SUPPORTS', 'REFUTES']:
+            coverage_ele = len(set(docs_predicted) & set(docs_gold)) / len(docs_gold)
+            coverage.append(coverage_ele)
+            coverage_all.append(coverage_ele)
+        else:
+            coverage_ele = len(set(docs_predicted) & set(docs_gold)) / len(docs_gold)
+            coverage_all.append(coverage_ele)
+    print("Recall Supports/Refutes:", average(coverage))
+    print("Recall All:", average(coverage_all))
+    return average(coverage_all)
 
 if __name__ == "__main__":
 
     parser = argparse.ArgumentParser()
-    parser.add_argument('--split', type=str, default='dev')
-    parser.add_argument('--max_page', type=int, default=5)
-    parser.add_argument('--max_sent', type=int, default=5)
-    parser.add_argument('--all', type=int, default=1)
+    parser.add_argument('--split', type=str)
     parser.add_argument('--input_path', type=str)
+    parser.add_argument('--max_page', type=int, default=1)
+    parser.add_argument('--max_sent', type=int, default=1)
+    parser.add_argument('--all', type=int, default=0)
     args = parser.parse_args()
+    split = args.split
 
-    in_path = '{0}/{1}.sentences.roberta.p{2}.s{3}.jsonl'.format(args.input_path, args.split, args.max_page, args.max_sent)
+    # q = 0
+    # q_all = 0
+    # score = 0
+    # score_all = 0
+    in_path = 'data/{0}.sentences.not_precomputed.p{1}.s{2}.jsonl'.format(split, args.max_page, args.max_sent)
 
     coverage = []
-    precision = []
     coverage_all = []
-    precision_all = []
-    annotation_processor = AnnotationProcessor('{0}/{1}.jsonl'.format(args.input_path, args.split))
+    # in_path = 'data/annotations/{0}.sentences.not_precomputed.p{1}.s{2}.jsonl'.format(split, args.max_page, args.max_sent)
+    annotation_processor = AnnotationProcessor('data/{}.jsonl'.format(args.split))
     if args.all == 0:
         annotation_by_id = {el.get_id(): el for el in annotation_processor if el.has_evidence() and el.get_evidence_type(flat=True) == EvidenceType.SENTENCE}
     else:
@@ -61,44 +95,30 @@ if __name__ == "__main__":
 
     with open(in_path,"r") as f:
         for idx,line in enumerate(f):
-            
             js = json.loads(line)
-            if idx == 0:
-                continue
             id = js['id']
-
+            if id not in annotation_by_id:
+                continue
             anno = annotation_by_id[id]
             docs_gold = list(set([t for t in anno.get_evidence(flat=True) if '_sentence_' in t]))
             if len(docs_gold) == 0:
                 continue
             # print(docs_gold)
-            docs_predicted = [item for item in js['predicted_sentences'] if '_sentence_' in item]
-            #docs_predicted =  [t[0] + '_' + t[1].replace('s_', 'sentence_') for t in js['predicted_sentences'][:5]]
-            #docs_predicted = js['predicted_sentences']
-            #docs_predicted = [t[0] + '_' +t[1] for t in js['predicted_evidence'] if 'sentence_' in t[1]]
+            docs_predicted =  [t[0] + '_' + t[1].replace('s_', 'sentence_') for t in js['predicted_sentences'][:5]]
             # docs_predicted =  [t[0] + '_' + t[1].replace('s_', 'sentence_') for t in js['predicted_sentences']]
             if anno.get_verdict() in ['SUPPORTS', 'REFUTES']:
                 coverage_ele = len(set(docs_predicted) & set(docs_gold)) / len(docs_gold)
-                if docs_predicted:
-                    precision_ele = len(set(docs_predicted) & set(docs_gold)) / len(docs_predicted)
-                else:
-                    precision_ele = 0
                 coverage.append(coverage_ele)
                 coverage_all.append(coverage_ele)
-                precision.append(precision_ele)
-                precision_all.append(precision_ele)
             else:
                 coverage_ele = len(set(docs_predicted) & set(docs_gold)) / len(docs_gold)
-                if docs_predicted:
-                    precision_ele = len(set(docs_predicted) & set(docs_gold)) / len(docs_predicted)
-                else:
-                    precision_ele = 0
                 coverage_all.append(coverage_ele)
-                precision_all.append(precision_ele)
-    print('Recall:')
     print(average(coverage))
     print(average(coverage_all))
-    print('Precision:')
-    print(average(precision))
-    print(average(precision_all))
 
+    # PYTHONPATH=src python src/baseline/retriever/eval_sentence_retriever.py --split dev --max_page 5 --max_sent 5
+
+    # baseline 0.53
+
+    # 0.6409232365145233
+    # 0.6254080351537985
